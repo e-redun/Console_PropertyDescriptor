6e1b1d75d1a354b24a111ac75da7d7a1ffd71ed3
e-redun <e.redun@mail.ru>
2024-02-01 19:15:18 +0500

    Program

--- Console_PropertyDescriptor/Program.cs
+++ Console_PropertyDescriptor/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsolePropertyDescriptor
 {
@@ -10,6 +6,30 @@ namespace ConsolePropertyDescriptor
     {
         static void Main(string[] args)
         {
+            // Создание источника данных (ViewModel)
+            SourceClassPD source = new SourceClassPD();
+
+            // Создание двух Представлений для одного источника
+            TargetClassPD target1 = new TargetClassPD("Первый", source, nameof(source.Number));
+            TargetClassPD target2 = new TargetClassPD("Второй", source, nameof(source.Number));
+
+            // Цикл для изменения Представления и Источника
+            do
+            {
+                Console.WriteLine();
+
+                // Ввод данных в привязку
+                Console.Write("Введите новое значение для target1.Value: ");
+                target1.Value = Console.ReadLine();
+
+                // Ввод данных в источник
+                Console.Write("Введите новое значение для source.Number (только ЦЕЛОЕ ЧИСЛО!): ");
+                source.Number = int.Parse(Console.ReadLine());
+
+                Console.Write("Нажмите Enter для Продолжения или Esc для Выхода.....");
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+
         }
     }
+
 }
